3952bc8e8a0b6e97ffaadb4fadc2427a5fc0c63f
ksenichen <ksenixen@mail.ru>
2015-11-19 02:30:18 +0200

    Reorganized code

--- pr_meta_widget.php
+++ pr_meta_widget.php
@@ -1,75 +1,76 @@
 <?php
-class pr_meta_widget extends WP_Widget {
 
-	function __construct() {
+class pr_meta_widget extends WP_Widget
+{
 
-		parent::__construct(
-		 'pr_meta_widget',
-		__('Product Meta Widget', 'btru_widget_domain'),	 
-		array( 'description' => __( 'Виджет для отображения метаданных продукта', 'btru_widget_domain' ), )
-		);
-	} 
+    function __construct()
+    {
 
-	public function widget( $args, $instance ) {
+        parent::__construct(
+            'pr_meta_widget',
+            'Product Meta Widget',
+            array('description' => 'Widget for product or service meta',)
+        );
+    }
 
-		global $post;
-		$product_name = get_post_meta($post->ID, '_name', true);
-	    $product_description = get_post_meta($post->ID, '_description', true);
-	    $product_price = get_post_meta($post->ID, '_price', true);
-	    $product_currency = get_post_meta($post->ID, '_currency', true);
-	    $image = get_post_meta($post->ID, "_my_image_upload", true);
+    public function widget($args, $instance)
+    {
 
-		if (!empty($product_name) && !empty($product_description) && !empty($product_price) && !empty($product_currency) && !empty($image)){
+        global $post;
+        $product_name = get_post_meta($post->ID, '_name', true);
+        $product_description = get_post_meta($post->ID, '_description', true);
+        $product_price = get_post_meta($post->ID, '_price', true);
+        $product_currency = get_post_meta($post->ID, '_currency', true);
+        $image = get_post_meta($post->ID, "_my_image_upload", true);
 
-			$title = apply_filters( 'widget_title', $instance['title'] );
-			echo $args['before_widget'];
-			if ( ! empty( $title ) )
-			 echo $args['before_title'] . $title . $args['after_title'];		
-			echo __( '<p> Name: ' . esc_attr($product_name) . '</p>
+        if (!empty($product_name) && !empty($product_description) && !empty($product_price) && !empty($product_currency) && !empty($image)) {
+
+            $title = apply_filters('widget_title', $instance['title']);
+            echo $args['before_widget'];
+            if (!empty($title))
+                echo $args['before_title'] . $title . $args['after_title'];
+            echo '<p> Name: ' . esc_attr($product_name) . '</p>
 					<p>Description: ' . esc_attr($product_description) . '</p>
 					<p>Image: <img src="' . esc_attr($image) . '"</></p>
 					<p>Price: ' . esc_attr($product_price) . '</p>
-					<p>Currency: ' . esc_attr($product_currency) . '</p>', 'btru_widget_domain' );
-			echo $args['after_widget'];
-		}
-		else return;
+					<p>Currency: ' . esc_attr($product_currency) . '</p>';
+            echo $args['after_widget'];
+        } else return;
 
-	}
+    }
 
- 	public function form( $instance ) {
+    public function form($instance)
+    {
 
-		if ( isset( $instance[ 'title' ] ) ) {
-		  $title = $instance[ 'title' ];
-		}
-		else {
-		  $title = __( 'Product or service meta', 'btru_widget_domain' );	
-	}
+        $title = isset($instance['title']) ? $instance['title'] : 'Product or service meta';
 
-	?>
+        ?>
 
-	<p>
-	<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
-	<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
-	</p>	
+        <p>
+            <label for="<?php echo $this->get_field_id('title'); ?>"><?php echo 'Title:'; ?></label>
+            <input id="<?php echo $this->get_field_id('title'); ?>"
+                   name="<?php echo $this->get_field_name('title'); ?>" type="text"
+                   value="<?php echo esc_attr($title); ?>"/>
+        </p>
 
-	<?php
-	}	     
+        <?php
+    }
 
-	// Обновление виджета
+    public function update($new_instance, $old_instance)
+    {
 
-	public function update( $new_instance, $old_instance ) {
+        $instance = ['title' => (isset($new_instance['title'])) ? strip_tags($new_instance['title']) : ''];
+        return $instance;
 
-	$instance = array();
-	$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
-	return $instance;
-
-	}
-
-} 
-
-function pr_meta_load_widget() {
-
-    register_widget( 'pr_meta_widget' );
+    }
 
 }
+
+function pr_meta_load_widget()
+{
+
+    register_widget('pr_meta_widget');
+
+}
+
 ?>
--- prodservmeta_plugin.php
+++ prodservmeta_plugin.php
@@ -4,33 +4,36 @@ Plugin Name: prodservmeta_plugin
 Author: Kseniia Loginova
 */
 
-function prod_meta_plugin_add_meta_box() {
+function prod_meta_plugin_add_meta_box()
+{
 
-	$screens = array( 'post', 'page' );
+    $screens = array('post', 'page');
 
-	foreach ( $screens as $screen ) {
+    foreach ($screens as $screen) {
 
-		add_meta_box(
-			'metaplugin_sectionid',
-			__( 'Product meta', 'myplugin_textdomain' ),
-			'plugin_meta_box_callback',
-			$screen,
-			'side'
-		);
-	}
+        add_meta_box(
+            'metaplugin_sectionid',
+            'Product meta',
+            'plugin_meta_box_callback',
+            $screen,
+            'side'
+        );
+    }
 }
-add_action( 'add_meta_boxes', 'prod_meta_plugin_add_meta_box' );
 
-function plugin_meta_box_callback( $post ) {
+add_action('add_meta_boxes', 'prod_meta_plugin_add_meta_box');
 
-	$product_name = get_post_meta($post->ID, '_name', true);
+function plugin_meta_box_callback($post)
+{
+
+    $product_name = get_post_meta($post->ID, '_name', true);
     $product_description = get_post_meta($post->ID, '_description', true);
     $product_price = get_post_meta($post->ID, '_price', true);
     $product_currency = get_post_meta($post->ID, '_currency', true);
-    wp_nonce_field( 'prod_meta_plugin_save_meta_box_data', 'prodplugin_meta_box_nonce' );
-    $image = get_post_meta($post->ID, "_my_image_upload", true);       
-     		
-	echo   '<!--Указывается схема Product.-->
+    wp_nonce_field('prod_meta_plugin_save_meta_box_data', 'prodplugin_meta_box_nonce');
+    $image = get_post_meta($post->ID, "_my_image_upload", true);
+
+    echo '<!--Указывается схема Product.-->
 	<div itemscope itemtype="http://schema.org/Product">
 		<!--В поле name указывается наименование товара.-->
 		<label>Name</label>
@@ -55,76 +58,86 @@ function plugin_meta_box_callback( $post ) {
 
 }
 
-function prod_plugin_media_lib_uploader_enqueue() {
+function prod_plugin_media_lib_uploader_enqueue()
+{
     wp_enqueue_media();
-    wp_register_script( 'media-lib-uploader-js', plugins_url( 'media-lib-uploader.js' , __FILE__ ), array('jquery') );
-    wp_enqueue_script( 'media-lib-uploader-js' );
+    wp_register_script('media-lib-uploader-js', plugins_url('media-lib-uploader.js', __FILE__), array('jquery'));
+    wp_enqueue_script('media-lib-uploader-js');
 }
+
 add_action('admin_enqueue_scripts', 'prod_plugin_media_lib_uploader_enqueue');
 
 
-function prod_meta_plugin_save_meta_box_data( $post_id ) {
+function prod_meta_plugin_save_meta_box_data($post_id)
+{
 
-	global $wpdb;
+    global $wpdb;
 
-	if (!isset($_POST['name']) || !isset($_POST['description']) || !isset($_POST['price']) || !isset($_POST['currency'])) 
-		return; 
+    if (!isset($_POST['name']) || !isset($_POST['description']) || !isset($_POST['price']) || !isset($_POST['currency']))
+        return;
 
-	//не происходит ли автосохранение? 
-	if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) 
-		return; 
+    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE)
+        return;
 
-	// не ревизию ли сохраняем? 
-	if (wp_is_post_revision($postID)) 
-		return; 
+    if (wp_is_post_revision($postID))
+        return;
 
-	check_admin_referer('prod_meta_plugin_save_meta_box_data', 'prodplugin_meta_box_nonce');
+    check_admin_referer('prod_meta_plugin_save_meta_box_data', 'prodplugin_meta_box_nonce');
 
-	require_once( ABSPATH . 'wp-admin/includes/image.php' );
-	require_once( ABSPATH . 'wp-admin/includes/file.php' );
-	require_once( ABSPATH . 'wp-admin/includes/media.php' );
+    require_once(ABSPATH . 'wp-admin/includes/image.php');
+    require_once(ABSPATH . 'wp-admin/includes/file.php');
+    require_once(ABSPATH . 'wp-admin/includes/media.php');
 
-	$product_name = sanitize_text_field($_POST['name']);
-	update_post_meta($post_id, '_name', $product_name);
+    $product_name = sanitize_text_field($_POST['name']);
+    update_post_meta($post_id, '_name', $product_name);
 
-	$product_description = sanitize_text_field($_POST['description']);
-	update_post_meta($post_id, '_description', $product_description);
-  
-	if ( !empty( $_POST['image'] ) ) {
-	  $image_url = $_POST['image'];
-	  $wpdb->insert( 'images', array( 'image_url' => $image_url ), array( '%s' ) ); 
-	}
-	update_post_meta($post_id, "_my_image_upload", $image_url);
+    $product_description = sanitize_text_field($_POST['description']);
+    update_post_meta($post_id, '_description', $product_description);
 
-	$product_price = sanitize_text_field($_POST['price']);
-	update_post_meta($post_id, '_price', $product_price);
+    if (!empty($_POST['image'])) {
+        $image_url = $_POST['image'];
+        $wpdb->insert('images', array('image_url' => $image_url), array('%s'));
+    }
+    update_post_meta($post_id, "_my_image_upload", $image_url);
 
-	$product_currency = sanitize_text_field($_POST['currency']);
-	update_post_meta($post_id, '_currency', $product_currency);
+    $product_price = sanitize_text_field($_POST['price']);
+    update_post_meta($post_id, '_price', $product_price);
+
+    $product_currency = sanitize_text_field($_POST['currency']);
+    update_post_meta($post_id, '_currency', $product_currency);
 }
 
-add_action( 'save_post', 'prod_meta_plugin_save_meta_box_data' );
+add_action('save_post', 'prod_meta_plugin_save_meta_box_data');
 
-require_once( ABSPATH . 'wp-content/plugins/prodservmeta_plugin/pr_meta_widget.php' );
-add_action( 'widgets_init', 'pr_meta_load_widget' );
+require_once(ABSPATH . 'wp-content/plugins/prodservmeta_plugin/pr_meta_widget.php');
+add_action('widgets_init', 'pr_meta_load_widget');
 
-function add_short_meta() {
-	global $post;
-	$product_name = get_post_meta($post->ID, '_name', true);
-    $product_description = get_post_meta($post->ID, '_description', true);
-    $product_price = get_post_meta($post->ID, '_price', true);
-    $product_currency = get_post_meta($post->ID, '_currency', true);
-    wp_nonce_field( 'prod_meta_plugin_save_meta_box_data', 'prodplugin_meta_box_nonce' );
-    $image = get_post_meta($post->ID, "_my_image_upload", true);  
-    $meta ='<div><strong>Product meta</strong>
+
+function add_short_meta($atts)
+{
+    global $post;
+    extract(shortcode_atts(array(
+        'id' => '',
+    ), $atts));
+
+    $atts['id'] = empty($atts['id']) ? $post->ID : $atts['id'];
+    $product_name = get_post_meta($atts['id'], '_name', true);
+    $product_description = get_post_meta($atts['id'], '_description', true);
+    $product_price = get_post_meta($atts['id'], '_price', true);
+    $product_currency = get_post_meta($atts['id'], '_currency', true);
+    $image = get_post_meta($atts['id'], "_my_image_upload", true);
+
+    if (!empty($product_name) && !empty($product_description) && !empty($product_price) && !empty($product_currency) && !empty($image)) {
+        $meta = '<div><strong>Product meta</strong>
 		    <br>Name: ' . esc_attr($product_name) . '
 			<br>Description: ' . esc_attr($product_description) . '
 			<br>Image: <img src="' . esc_attr($image) . '"</>
 			<br>Price: ' . esc_attr($product_price) . '
 			<br>Currency: ' . esc_attr($product_currency) . '
 		    </div>';
-	return $meta;
+        return $meta;
+    }
 }
- 
-add_shortcode( 'add_short_meta', 'add_short_meta' );
+
+add_shortcode('add_short_meta', 'add_short_meta');
 ?>
